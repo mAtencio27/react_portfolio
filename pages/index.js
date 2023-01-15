6e45dfca98326b7f1aaa4613489ed0787d352de7
mAtencio27 <mAtencio27@hotmail.com>
2023-01-16 01:39:37 +0900

    implemented: First section of page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import {AiFillLinkedin, AiFillGithub, AiFillMail} from 'react-icons/ai';
+import Image from 'next/image';
 
 
 export default function Home() {
@@ -10,9 +13,33 @@ export default function Home() {
         {/*<meta name="viewport" content="width=device-width, initial-scale=1" />*/}
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section>
-          <h1 className="bg-blue-700">Hello</h1>
+      <main className='bg-white px-10'>
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className='text-xl font-burtons'>developedbyMarcus</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className='curser-pointer text-2xl'/>
+              </li>
+              <li>
+                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border rounded-md ml-8' href="#">Resume</a>
+              </li>
+
+            </ul>
+          </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium'>Marcus Atencio</h2>
+            <h3 className='text-2xl py-2'>Full-Stack Developer.</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>
+              Full Stack Developer based in Tokyo. Always looking
+              to grow and improve. Continue below to view my experience.
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-500'>
+            <a><AiFillLinkedin/></a>
+            <a><AiFillGithub/></a>
+            <a><AiFillMail/></a>
+          </div>
         </section>
       </main>
     </>
